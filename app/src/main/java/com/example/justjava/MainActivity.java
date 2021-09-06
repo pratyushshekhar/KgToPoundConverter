0a6b85623ee09bae507950b6618d9d441a2ee7e8
Pratyush Kumar Shekhar <shekharofficial311@gmail.com>
2021-09-06 18:42:34 +0530

    Kilogram-To-Pound-Converter App

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private TextView textView2;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+        textView2 = findViewById(R.id.textView3);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Process Executed Successfully", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                long  kg = Integer.parseInt(s);
+                double pound = 2.205* kg;
+                textView2.setText("The corresponding Pound value is " + pound);
+            }
+        });
+    }
+}
